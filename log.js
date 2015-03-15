4159776308ea2b7a53bab85beb7afb7ec5d7b51d
vishal <vishal_s_yadav@yahoo.com>
2015-03-15 14:40:42 -0700

    Implemented makeCopyWithFreshNames

--- log.js
+++ log.js
@@ -3,8 +3,25 @@
 //         {Clause, Var}.prototype.rewrite(subst)
 // -----------------------------------------------------------------------------
 
+function makeSingleCopy(c) {
+	if (c.args.length == 0) {
+		return new Clause(c.name);
+	}
+	var args = [];
+	c.args.map(function (v) {
+		if (v instanceof Var) {
+			args.push(new Var("__" + c.args[0].name + "__"));
+		} else {
+			args.push(makeSingleCopy(v));
+		}
+	});
+	return new Clause(c.name, args);
+}
+
 Rule.prototype.makeCopyWithFreshVarNames = function() {
-  throw new TODO("Rule.prototype.makeCopyWithFreshVarNames not implemented");
+	var new_head = makeSingleCopy(this.head);
+	var new_body = this.body.map(makeSingleCopy);
+	return new Rule(new_head, new_body);
 };
 
 Clause.prototype.rewrite = function(subst) {
